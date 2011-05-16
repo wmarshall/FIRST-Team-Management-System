9f0909634bf955612920657ccc56bbe6c8eb8003
Patrick Jameson <patrickejameson@gmail.com>
2011-05-15 20:26:04 -0400

    Added basic structure for storage and retrieval of comments. You can't post comments yet and I want to restructure how issues are stored but I figured it would be a good time to commit.

--- team.py
+++ team.py
@@ -1,8 +1,9 @@
-import web, re
+import web, time
 
 urls = (
-	"/", "index",
-	"/issues", "issues",
+    "/", "index",
+    "/issues", "issues",
+    "/getcomments", "getcomments"
 )
 app = web.application(urls, globals())
 
@@ -15,38 +16,62 @@ class index:
         return render.index(modules)
 
 class issues:
-	def GET(self):
-		issuesfile = open(root + 'issues', 'r')
-		issuelines = issuesfile.readlines()
-		issuesfile.close()
+    def GET(self):
+        starttime = time.time()
+        issuesfile = open(root + 'issues', 'r')
+        issuelines = issuesfile.readlines()
+        issuesfile.close()
 
-		i = 0
-		appending = ''
-		while i < len(issuelines):
-			issuelines[i].replace('\n', '')
-			if i % 4 == 0:
-				appending += '<tr><td width="600px">' + issuelines[i] + '</td>'
-			else:
-				appending += '<td>' + issuelines[i] + '</td>'
-			i += 1
+        i = 0
+        appending = ''
+        subissues = {'programming': '', 'endeffector': '', 'electrical': '', 'drivetrain': '', 'cad': ''}
+        
+        while i < len(issuelines):
+            issuelines[i] = issuelines[i].replace('\n', '')
+            if i % 5 == 0:
+                subteam = issuelines[i]
+                i += 1
+                issuelines[i] = issuelines[i].replace('\n', '')
+                appending += '<tr class="issue" id="' + str(int(i/4)+1) + '"><td width="600px">' + issuelines[i] + '</td>'
+            else:
+                appending += '<td width="100px">' + issuelines[i] + '</td>'
+            i += 1
 
-			if i % 4 == 0:
-				appending += '</tr>'
-		return render.issues(modules, appending)
+            if i % 5 == 0:
+                appending += '</tr><tr style="display:none" class="issuedetail" id="comments' + str(int(i/4)) + '"><td width="600px"></td></tr>'
+                subissues[subteam] += appending
+                appending = ''
+        print 'return time: ' + str(time.time() - starttime) + '\n'
+        return render.issues(modules, subissues)
 
-	def POST(self):
-		issuedata = web.input()
-		
-		issuesfile = open(root + 'issues', 'r')
-		old = issuesfile.read()
-		issuesfile.close
-		
-		issuesfile = open(root + 'issues', 'w')
-		issuesfile.write(issuedata['issuetext'] + '\n' + issuedata['issuepriority'] + '\n5/11\nPat')
-		if old != "":
-			issuesfile.write('\n')
-		issuesfile.write(old)
-		issuesfile.close()
+    def POST(self):
+        issuedata = web.input()
+        
+        issuesfile = open(root + 'issues', 'r')
+        old = issuesfile.read()
+        oldlinenum = len(issuesfile.readlines())
+        issuesfile.close()
+        
+        issuesfile = open(root + 'issues', 'w')
+        issuedata['issuetext'] = issuedata['issuetext'].replace('\n', '<br />')
+        issuesfile.write(issuedata['issuesubteam'] + '\n' + issuedata['issuetext'] + '\n' + issuedata['issuepriority'] + '\n5/11\nPat')
+        if old != "":
+            issuesfile.write('\n')
+        issuesfile.write(old)
+        issuesfile.close()
+        return str(oldlinenum+1)
+
+class getcomments:
+    def POST(self):
+		try:
+			commentfile = open(root + 'comments/comment' + web.input()['id'])
+			commentlines = commentfile.readlines()
+			commenthtml = ''
+			for i in range(0, len(commentlines), 2):
+				commenthtml += '<div class="commenttext">' + commentlines[i] + '</div><div class="commentuser">-' + commentlines[i+1] + '</div>'
+		except:
+			commenthtml = ''
+		return modules.issueviewer(commenthtml)
 
 if __name__ == "__main__":
     app.run()
